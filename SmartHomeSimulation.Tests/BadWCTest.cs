ed879db643a07b5d73eae827924f6ea2d363c8bd
David Hafner <david.hafner@tingo.ch>
2025-10-31 13:44:42 +0100

    added test project
    Signed-off-by: David Hafner <david.hafner@tingo.ch>

---
+++ SmartHomeSimulation.Tests/BadWCTest.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using M320_SmartHome;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SmartHomeSimulation.Tests;
+
+[TestClass]
+[TestSubject(typeof(BadWC))]
+public class BadWCTest
+{
+
+    [TestMethod]
+    public void METHOD()
+    {
+        
+    }
+}
